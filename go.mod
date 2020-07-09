ad4555ee704112c0f2debd596b41efae7dfcc017
tsuboi shotaro <jack007xx@gmail.com>
2020-07-09 22:30:40 +0900

    responseまで完成

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/jack007zz/nakamiru-kun
+
+go 1.14
+
+require (
+	github.com/TylerBrock/colorjson v0.0.0-20200706003622-8a50f05110d2
+	github.com/fatih/color v1.9.0 // indirect
+	github.com/hokaccha/go-prettyjson v0.0.0-20190818114111-108c894c2c0e // indirect
+	github.com/mattn/go-colorable v0.1.7 // indirect
+	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
+)
---
+++ nakamiru-kun.go
@@ -0,0 +1,79 @@
+package nakamirukun
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"text/template"
+
+	"github.com/TylerBrock/colorjson"
+)
+
+type req struct {
+	To          string
+	Method      string
+	URL         string
+	ContentType string
+	Query       string
+	Body        string
+}
+
+var logTemplateString = `
+Nakamiru-kun found here!!
+
+[Reqest]
+Method :      {{.Method}}
+To     :      {{.To}}
+URL    :      {{.URL}}
+
+Content-Type :    {{.ContentType}}
+
+Query :
+{{.Query}}
+
+Body :
+{{.Body}}
+`
+
+func Nakamiru(aHandler http.Handler) http.Handler {
+	return http.HandlerFunc(func(aWriter http.ResponseWriter, aRequest *http.Request) {
+		tBody, tError := ioutil.ReadAll(aRequest.Body)
+		if tError != nil {
+			print("[Nakamiru_kun panic!!]")
+			panic(tError)
+		}
+		var tJSONBody map[string]interface{}
+		json.Unmarshal(tBody, &tJSONBody)
+		tBodyFormatter := colorjson.NewFormatter()
+		tBodyFormatter.Indent = 2
+
+		tPrettyBody, tError := tBodyFormatter.Marshal(tJSONBody)
+
+		tReq := &req{
+			Method:      aRequest.Method,
+			To:          aRequest.Host + " [" + aRequest.RemoteAddr + "]",
+			URL:         aRequest.URL.Path,
+			ContentType: aRequest.Header.Get("Content-Type"),
+			Body:        string(tPrettyBody),
+			Query:       aRequest.URL.RawQuery,
+		}
+
+		tTemplate, tError := template.New("tReq").Parse(logTemplateString)
+		if tError != nil {
+			print("[Nakamiru_kun panic!!]")
+			panic(tError)
+		}
+
+		tBuf := new(bytes.Buffer)
+		tError = tTemplate.Execute(tBuf, tReq)
+		if tError != nil {
+			print("[Nakamiru_kun panic!!]")
+			panic(tError)
+		}
+
+		log.Printf(tBuf.String())
+		aHandler.ServeHTTP(aWriter, aRequest)
+	})
+}
